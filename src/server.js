ab10aafb3c68636d127e31e0c9f040328ba9af13
G <gregor@gregorfra.me>
2020-03-03 00:03:50 +0000

    pre-heroku-deployment

--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@ const app = require("./app");
 const imgurController = require("./controllers/imgur-cards");
 const { logger } = require("./logger");
 
-const server = app.listen(config.get("app.port"), async () => {
+const PORT = process.env.PORT || config.get("app.port");
+
+const server = app.listen(PORT, async () => {
   logger.info(`Application running on port ${config.get("app.port")}`);
   await imgurController.savePokemonImgurIds();
   await imgurController.savePersonalImgurIds();
