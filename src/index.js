092bd84aac57a9b983de4ec0e27b97b4f38f4610
G <gregor@gregorfra.me>
2020-03-03 01:21:42 +0000

    adjusting config for heroku

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const config = require("config");
-require("./db/mongoose")(process.env.mongoURL || config.get("mongo.url"));
+require("./db/mongoose")(process.env.MONGODB_URI || config.get("mongo.url"));
 
 require("./server");
